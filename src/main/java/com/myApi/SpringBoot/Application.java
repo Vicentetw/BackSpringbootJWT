b3f184d5609b0883fd6fd7db171fdd9b6eb6c40f
Vicentetw <75254133+Vicentetw@users.noreply.github.com>
2023-02-10 09:01:16 -0300

    application and main

--- src/main/java/com/myApi/SpringBoot/Application.java
+++ src/main/java/com/myApi/SpringBoot/Application.java
@@ -26,7 +26,9 @@ public class Application {
     public CorsFilter corsFilter() {
       CorsConfiguration corsConfiguration = new CorsConfiguration();
       corsConfiguration.setAllowCredentials(true);
-      corsConfiguration.setAllowedOrigins(Arrays.asList("https://perrotta-vicente.web.app"));
+      //corsConfiguration.setAllowedOrigins(Arrays.asList("https://perrotta-vicente.web.app"));
+      //corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+      corsConfiguration.setAllowedOrigins(Arrays.asList("*"));
       corsConfiguration.setAllowedHeaders(Arrays.asList("Origin",
       "Access-Control-Allow-Origin", "Content-Type", "Accept", "Authorization",
       "Origin, Accept", "X-Request-With", "Access-Control-Request-Method",
